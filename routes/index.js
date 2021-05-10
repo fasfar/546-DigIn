8263a6212f35dfbf09b3936a9defc81bfae1fe23
Suzy Shailesh <sshailes@stevens.edu>
2021-05-10 15:12:35 -0400

    recipe functionality added

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const recipesRoutes = require('./recipes');
+
+
+const constructorMethod = (app) => {
+  app.use('/recipes', recipesRoutes);
+
+  app.use('*', (req, res) => {
+    res.status(404).json({ error: 'Not found' });
+  });
+};
+
+module.exports = constructorMethod;

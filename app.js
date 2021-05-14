0281260f263f4d840fdb28a833ca7452ec5c60a0
Suzy Shailesh <sshailes@stevens.edu>
2021-05-14 00:58:34 -0400

    added all likes and comments functionality

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+var hbs = exphbs.create({});
+
+hbs.handlebars.registerHelper('each_upto', function(ary, max, options) {
+  if(!ary || ary.length == 0)
+      return options.inverse(this);
+
+  var result = [ ];
+  for(var i = 0; i < max && i < ary.length; ++i)
+      result.push(options.fn(ary[i]));
+  return result.join('');
+});
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 

11132c7bd31397a75dbf693627f9c3317a06632f
Michael McCreesh <mmccree1@stevens.edu>
2021-05-14 23:59:22 -0400

    recipes appear newest to oldest

--- data/users.js
+++ data/users.js
@@ -304,6 +304,7 @@ const getFeed = async function getFeed(id){
                 recipesBy.push(recipe);
             }
         });
+        recipesBy.reverse();
         return recipesBy;
 
     }catch(e){
